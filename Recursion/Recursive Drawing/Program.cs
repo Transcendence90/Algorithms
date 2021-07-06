2a2f12a109c16d464dba10ecb4c38553a7d74a85
Martin Katov <48762563+Transcendence90@users.noreply.github.com>
2021-07-06 22:56:57 +0300

    solve RecursiveArraySum and RecursiveDrawing

---
+++ Recursion/Recursive Drawing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Recursive_Drawing
+{
+    class Program
+    {
+        static void PrintFigure(int n)
+        {
+            if (n == 0) // Bottom of the recursion
+                return;
+
+            // Pre-action: print n asterisks
+            Console.WriteLine(new string('*', n));
+
+            // Recursive call: print figure of size n-1
+            PrintFigure(n - 1);
+
+            // Post-action: print n hashtags
+            Console.WriteLine(new string('#', n));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintFigure(n);
+        }
+    }
+}
