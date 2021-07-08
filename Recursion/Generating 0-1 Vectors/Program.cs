d4aa23a34ae70c1c72c9fbeac0989405cbce74ff
Martin Katov <48762563+Transcendence90@users.noreply.github.com>
2021-07-08 23:19:20 +0300

    Add solution for Generating 0-1 vectors and Recursive factorial. Also add all projects in one new blanc solution.

---
+++ Recursion/Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generating_0_1_Vectors
+{
+    class Program
+    {
+        static void Gen01(int index, int[] vector)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    vector[index] = i;
+                    Gen01(index + 1, vector);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+
+            Gen01(0, vector);
+        }
+    }
+}
---
+++ Recursion/Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static long Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
+        }
+
+        protected static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(num));
+        }
+    }
+}
